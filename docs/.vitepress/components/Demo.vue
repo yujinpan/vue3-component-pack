f91344bee6cdd8ef4ff2e27ac3aaa53e4823c544
yujinpan <1192878390@qq.com>
2022-08-02 16:10:53 +0800

    docs: update demo container style

--- docs/.vitepress/components/Demo.vue
+++ docs/.vitepress/components/Demo.vue
@@ -1,9 +1,11 @@
 <template>
-  <component :is="component" />
-  <details v-if="code" class="custom-block details">
-    <summary>Click me to view the code</summary>
-    <span v-html="code"></span>
-  </details>
+  <div class="demo-wrapper">
+    <component :is="component" />
+    <details v-if="code" class="custom-block details">
+      <summary>Click me to view the code</summary>
+      <span v-html="code"></span>
+    </details>
+  </div>
 </template>
 
 <script>
@@ -20,3 +22,14 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@import '../styles/var.scss';
+
+.demo-wrapper {
+  border: 1px solid var(--vp-c-divider-light);
+  border-radius: $spacing-base;
+  padding: $spacing-medium $spacing-medium 0;
+  margin: $spacing-medium 0;
+}
+</style>
