76bd20792fb9097a2889fa71208447ecb1c2a9b4
yujinpan <1192878390@qq.com>
2022-08-02 13:31:22 +0800

    chore: root dir move to dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url';
+import * as path from 'path';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
@@ -6,6 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: path.resolve(__dirname, './dev/'),
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
