64158057bf6891a21696a0dd0c823a4b334150dd
yujinpan <1192878390@qq.com>
2022-09-08 10:37:22 +0800

    chore: use eslint import plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution');
+const path = require('path');
 
 module.exports = {
   root: true,
@@ -10,14 +11,71 @@ module.exports = {
     '@vue/eslint-config-prettier',
   ],
   rules: {
+    // eslint http://eslint.cn/docs/rules/
+    'no-unused-vars': 'error',
+    'no-debugger': 'error',
+    'no-console': 'error',
+    eqeqeq: ['error', 'always'],
+
+    // prettier https://prettier.io/docs/en/options.html
     'prettier/prettier': [
       'error',
       {
         singleQuote: true,
         arrowParens: 'always',
         semi: true,
+        trailingComma: 'all',
       },
     ],
+
+    // vue
     'vue/multi-word-component-names': 'off',
+
+    // typescript https://typescript-eslint.io/rules/
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          '{}': false,
+        },
+      },
+    ],
+
+    // import https://github.com/import-js/eslint-plugin-import#rules
+    'import/no-useless-path-segments': 'error',
+    'import/first': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent', 'sibling', 'index', 'type'],
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc' },
+      },
+    ],
+    'import/newline-after-import': 'error',
+  },
+
+  plugins: [
+    // https://github.com/import-js/eslint-import-resolver-typescript#configuration
+    'import',
+  ],
+
+  settings: {
+    // https://github.com/import-js/eslint-import-resolver-typescript#configuration
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: path.resolve(__dirname, 'tsconfig.json'),
+      },
+    },
   },
 };
