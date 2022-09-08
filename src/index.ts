1437768a67a3354fb61fb8696fd0887d6d6772d4
yujinpan <1192878390@qq.com>
2022-09-08 10:39:04 +0800

    lint with new rules

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // 主组件
+import type { App } from 'vue';
+
 import HelloWorld from '@/components/HelloWorld.vue';
 import './styles/index.scss';
-import type { App } from 'vue';
 
 export const install = (vue: App) => {
   vue.component('HelloWorld', HelloWorld);
