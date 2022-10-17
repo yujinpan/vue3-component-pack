73213ab46c6d8d803126804e4b610341bd961108
yujinpan <1192878390@qq.com>
2022-10-17 14:07:50 +0800

    Merge from vue-component-pack

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,32 @@
 /// <reference types="vitest" />
+
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import { fileURLToPath, URL } from 'node:url';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
+
+const alias = {
+  '@': resolve('src'),
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: 'dev',
   plugins: [vue(), vueJsx()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('src', import.meta.url)),
+    alias,
+    extensions: ['.vue', '.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // resolve start path for "~", like: "~external/style/var.scss"
+        importer: (url: string) => {
+          return {
+            file: parseAlias(url.startsWith('~') ? url.slice(1) : url, alias),
+          };
+        },
+      },
     },
   },
   // https://vitest.dev/config/
@@ -18,3 +34,17 @@ export default defineConfig({
     dir: 'src',
   },
 });
+
+function parseAlias(file: string, alias: Record<string, string>) {
+  const key = Object.keys(alias).find(
+    (item) => file.startsWith(item + '/') || file === item,
+  );
+  if (key) {
+    if (key === file) {
+      return alias[key];
+    } else {
+      return file.replace(key, alias[key]);
+    }
+  }
+  return file;
+}
